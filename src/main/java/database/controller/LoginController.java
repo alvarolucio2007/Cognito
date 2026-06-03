81cefd5a57fa8c5b1bbf09c9e8c15352b9ada72d
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-06-03 17:34:47 -0300

    feat: Adicionando função de login e checagem de senha por e-mail.

--- src/main/java/database/controller/LoginController.java
+++ src/main/java/database/controller/LoginController.java
@@ -1,5 +1,11 @@
 package database.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import database.conn.databaseConn;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,18 +15,43 @@ import javafx.scene.control.TextField;
 
 public class LoginController {
 
-    @FXML private TextField emailField;
-    @FXML private PasswordField senhaField;
-    @FXML private Button botaoLogin;
+  @FXML
+  private TextField emailField;
+  @FXML
+  private PasswordField senhaField;
+  @FXML
+  private Button botaoLogin;
 
-    @FXML private void login() {
-        // TODO: implementar autenticação
+  private boolean login() {
+    String emailDigitado = emailField.getText();
+    String senhaDigitada = senhaField.getText();
+    try {
+      String senhaRecebida = getSenhaEmail(emailDigitado);
+      if (senhaRecebida == null) {
+        return false;
+      }
+      return senhaDigitada.equals(senhaRecebida);
+    } catch (SQLException e) {
+      // TODO: Colocar algo aqui, nao sei direito, vou fazer depois...
+      return false;
     }
 
-    @FXML private void irParaCadastro() throws Exception {
-        Parent root = FXMLLoader.load(
-            getClass().getResource("/fxml/Cadastro.fxml")
-        );
-        botaoLogin.getScene().setRoot(root);
+  }
+
+  @FXML
+  private void irParaCadastro() throws Exception {
+    Parent root = FXMLLoader.load(
+        getClass().getResource("/fxml/Cadastro.fxml"));
+    botaoLogin.getScene().setRoot(root);
+  }
+
+  public String getSenhaEmail(String email) throws SQLException {
+    String query = "SELECT usuario_senha FROM usuario WHERE usuario_email = ?";
+    try (Connection conn = databaseConn.connect(); PreparedStatement stmt = conn.prepareStatement(query)) {
+      stmt.setString(1, email);
+      ResultSet rs = stmt.executeQuery();
+      return rs.next() ? rs.getString("usuario_senha") : null;
     }
+
+  }
 }
