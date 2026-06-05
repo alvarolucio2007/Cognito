2c0d6c66c6ac61ba4f00bcc9253c4030a030ed45
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-06-05 15:58:26 -0300

    feat: Ajustando HelloFX, removendo StageStyle (não utilizado)

--- src/main/java/javaFX/HelloFX.java
+++ src/main/java/javaFX/HelloFX.java
@@ -5,25 +5,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class HelloFX extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setResizable(false);
-        stage.setTitle("Cognito");
-        stage.setWidth(440);
-        stage.setHeight(950);
-        // stage.initStyle(StageStyle.TRANSPARENT);
+  @Override
+  public void start(Stage stage) throws Exception {
+    stage.setResizable(false);
+    stage.setTitle("Cognito");
+    stage.setWidth(440);
+    stage.setHeight(950);
+    // stage.initStyle(StageStyle.TRANSPARENT);
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
