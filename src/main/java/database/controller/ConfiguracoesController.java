61a37c5df4c54cf8929c9cdfe00b28a2a647b6bd
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-06-05 14:30:02 -0300

    feat: Começando a implementacao de ConfiguracoesController

---
+++ src/main/java/database/controller/ConfiguracoesController.java
@@ -0,0 +1,17 @@
+package database.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class ConfiguracoesController {
+  @FXML
+  private CheckBox altoContrasteField;
+  @FXML
+  private CheckBox textoAmpliadoField;
+  @FXML
+  private TextField sensibilidadeToqueField;
+  @FXML
+  private CheckBox modoVozField;
+
+}
