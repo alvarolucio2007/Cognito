869b081384227b13eddf86ede47c0c8d6ca5e19f
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-06-05 14:59:38 -0300

    feat: Colocando função salvarConfigurações e verificarFloat (verifica float) e initalize

--- src/main/java/database/controller/ConfiguracoesController.java
+++ src/main/java/database/controller/ConfiguracoesController.java
@@ -2,6 +2,7 @@ package database.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class ConfiguracoesController {
@@ -10,8 +11,39 @@ public class ConfiguracoesController {
   @FXML
   private CheckBox textoAmpliadoField;
   @FXML
-  private TextField sensibilidadeToqueField;
+  private TextField sensibilidadeToqueField; // Aliás, como que faríamos esse sensibilidadeToque? Literamente n tem como
+                                             // controlar a sensibiliade no mouse kkkkkkkkkkkkkkkkkkkk
   @FXML
   private CheckBox modoVozField;
+  @FXML
+  private Label labelAviso;
+
+  public void initialize() { // essa bomba (provavelmente) deve prevenir que o usuário coloque texto aonde só
+                             // pode ter
+                             // número. Eu não confio. Preciso testar.
+    sensibilidadeToqueField.textProperty().addListener((observable, oldValue, newValue) -> {
+      if (!newValue.matches("\\d*(\\.\\d*)?")) {
+        textoAmpliadoField.setText(oldValue);
+      }
+    });
+  }
+
+  @FXML
+  private void salvarConfiguracoes() {
+    boolean altoContraste = altoContrasteField.isSelected();
+    boolean textoAmpliado = textoAmpliadoField.isSelected();
+    float sensibilidadeToque = verificarFloat(sensibilidadeToqueField.getText().trim());
+    boolean modoVoz = modoVozField.isSelected();
+  } // n vai ter muito uso isso...
+
+  private float verificarFloat(String string) {
+    try {
+      return Float.parseFloat(string);
+    } catch (NumberFormatException e) {
+      // TODO: Colocar lá no XML... Talvez? Acho que nem precise se eu colocar uma
+      // verificação
+      return -1;
+    }
+  }
 
 }
