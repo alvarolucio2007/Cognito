71cf0d3501f371159dd0eb2339ef7e01399f423e
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-06-05 15:35:10 -0300

    feat: Começando AtividadeController

---
+++ src/main/java/database/controller/AtividadeController.java
@@ -0,0 +1,12 @@
+package database.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class AtividadeController {
+  @FXML
+  private TextField atividadePergunta;
+  @FXML
+  private RadioButton atividadeResposta;
+}
